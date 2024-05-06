71075ae737b7ec16bdfad946a8aa8ecc0cc9f63d
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-05-06 17:49:44 +0200

    wip

--- scripts/generate.js
+++ scripts/generate.js
@@ -4,37 +4,28 @@ import cp from "child_process";
 import path from "path";
 import { fileURLToPath } from "url";
 
-main();
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const workspaceRoot = path.resolve(dirname, "..");
 
-function main() {
-  if (Number(process.env.nested ?? 0) !== 0) {
-    return;
-  }
+const options = { shell: true, stdio: "inherit", env: { ...process.env, nested: "1" } };
 
-  const dirname = path.dirname(fileURLToPath(import.meta.url));
-  const workspaceRoot = path.resolve(dirname, "..");
+const names = ["reverse-api"];
 
-  const options = { shell: true, stdio: "inherit", env: { ...process.env, nested: "1" } };
+for (const name of names) {
+  cp.execFileSync(
+    "oa42-generator",
+    [
+      "package",
+      path.resolve(workspaceRoot, "specifications", `${name}.yaml`),
+      "--package-directory",
+      path.resolve(workspaceRoot, "generated", "npm", name),
+      "--package-name",
+      name,
+      "--package-version",
+      "0.1.0",
+    ],
+    options,
+  );
 
-  const names = ["reverse-api"];
-
-  for (const name of names) {
-    cp.execFileSync(
-      "oa42-generator",
-      [
-        "package",
-        path.resolve(workspaceRoot, "specifications", `${name}.yaml`),
-        "--package-directory",
-        path.resolve(workspaceRoot, "generated", "npm", name),
-        "--package-name",
-        name,
-        "--package-version",
-        "0.1.0",
-      ],
-      options,
-    );
-
-    cp.execFileSync("npm", ["--workspace", name, "run", "build"], options);
-    cp.execFileSync("npm", ["--workspace", name, "install"], options);
-  }
+  cp.execFileSync("npm", ["--workspace", name, "run", "build"], options);
 }
