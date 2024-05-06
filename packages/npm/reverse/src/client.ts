b914c56333842fd3089c951838856f34d9068763
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-05-06 16:57:15 +0200

    some comments

--- packages/npm/reverse/src/client.ts
+++ packages/npm/reverse/src/client.ts
@@ -3,17 +3,23 @@ import * as api from "reverse-api";
 main();
 
 function main() {
+  // Add event listener for sub events
   window.addEventListener("submit", (event) => {
+    // prevent the submit from navigating
     event.preventDefault();
 
+    // wrap async task
     task().catch((error) => alert(error));
 
     async function task() {
+      // set a busy class to indicate that we are doing something
       window.document.body.className = "busy";
       try {
         const form = event.target as HTMLFormElement;
+        // assume our form has one element that is an input
         const input = form.elements.item(0) as HTMLInputElement;
 
+        // call the api
         const result = await api.reverse(
           {
             contentType: "text/plain",
@@ -23,14 +29,18 @@ function main() {
           { baseUrl: new URL("/", window.document.location.href) },
         );
 
+        // verify response status
         if (result.status !== 200) {
           throw new Error("unexpected status");
         }
 
+        // get the result
         const resultValue = await result.value();
 
+        // alert the result, a reversed string!
         alert(resultValue);
       } finally {
+        // unset busy class, we are done
         window.document.body.className = "";
       }
     }
