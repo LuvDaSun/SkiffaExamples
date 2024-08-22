0ba5860dfd0b8f6bf59f4ec8a322d5c51146b6ca
Israel Ojiefoh <israelmayowa580@gmail.com>
2024-08-22 13:35:12 -0700

    No changes really

--- packages/npm/todo/src/operation-handlers/todo.ts
+++ packages/npm/todo/src/operation-handlers/todo.ts
@@ -1,8 +1,11 @@
 import * as api from "todo-api";
 import { CommandHandlers } from "../commandHandlers/commandHandlers.js";
+import { QueryHandler } from "../queryHandler/queryHandler.js";
 
 import CreateTodo from "../commands/createTodo.js";
+import DeleteTodo from "../commands/deleteTodo.js";
 import updateTodo from "../commands/updateTodo.js";
+import listTodoItems from "../query/listTodoItems.js";
 
 export const addTodoItem: api.server.AddTodoItemOperationHandler<{}> = async (todo) => {
   const createTodoCommand = new CreateTodo(todo.description);
@@ -34,6 +37,29 @@ export const modifyTodoItem: api.server.ModifyTodoItemOperationHandler<{}> = asy
 };
 
 
+export const listTodoItem: api.server.ListTodoItemsOperationHandler<{}> = async (todo) => {
+  
+  const listTodoQuery = new listTodoItems(todo)
+  const listTodoHandlers = new QueryHandler();
+
+ const listTodo = await listTodoHandlers.listTodoItems(listTodoQuery);
+
+ // Not returning anything because of the issue i have with the todo parameter returning any
+  // const todoItem = {
+  //   description: listTodo.todoName,
+  //   id: updatedTodo.todoId,
+  //   done: updatedTodo.todoIsDone,
+  // };
+  // return todoItem;
+};
+
+
 export const deleteTodoItem:api.server.DeleteTodoItemOperationHandler<{}>= async(todo)=>{
 
+  const deleteTodoCommand = new DeleteTodo(todo.id)
+  const deleteTodoHandler = new CommandHandlers()
+
+  const deletedTodo = deleteTodoHandler.deleteTodoItem(deleteTodoCommand)
+
+  return deletedTodo
 }
