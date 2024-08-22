459742aaaef181b8e2a6a1c35f541c9131f0e743
Israel Ojiefoh <israelmayowa580@gmail.com>
2024-08-22 14:43:59 -0700

    Wrote test for List and delete Todo

--- packages/npm/todo/src/operation-handlers/todo.test.ts
+++ packages/npm/todo/src/operation-handlers/todo.test.ts
@@ -45,3 +45,50 @@ test("update todo", async () => {
   assert.equal(result.done, false);
   assert.equal(typeof result.id, "number");
 });
+
+
+test("List todo", async () => {
+  const server = new api.server.Server();
+  server.registerAddTodoItemOperation(TodoOperationHandler.addTodoItem);
+  server.registerListTodoItemsOperation(TodoOperationHandler.listTodoItem); // Register the update operation
+  await using listener = await api.lib.listen(server);
+  const baseUrl = new URL(`http://localhost:${listener.port}`);
+
+  // First, create a todo item to update
+  const createdResult = await api.client.addTodoItem({ description: "InitialTodo " }, { baseUrl });
+  const description = createdResult.description;
+
+  const result = await api.client.listTodoItems(
+    { description: description },
+      
+  );
+
+  // Log the result to see what's returned
+  console.log("Returned result:", result);
+
+  // Check if the result matches the expected format
+   assert.equal(typeof result, 'string');
+});
+
+test("delete todo", async () => {
+  const server = new api.server.Server();
+  server.registerAddTodoItemOperation(TodoOperationHandler.addTodoItem);
+  server.registerDeleteTodoItemOperation(TodoOperationHandler.deleteTodoItem); // Register the update operation
+  await using listener = await api.lib.listen(server);
+  const baseUrl = new URL(`http://localhost:${listener.port}`);
+
+  // First, create a todo item to update
+  const createdResult = await api.client.addTodoItem({ description: "InitialTodo " }, { baseUrl });
+  const todoId = createdResult.id;
+
+  const result = await api.client.deleteTodoItem(
+    { id: todoId },
+     { baseUrl },
+  );
+
+  // Log the result to see what's returned
+  console.log("Returned result:", result);
+
+  // Check if the result matches the expected format
+   assert.equal(typeof result !== 'object', true);
+});
